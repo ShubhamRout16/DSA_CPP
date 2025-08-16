871858ee2cef6cec31c54b9e8707e94c134e30b5
ShubhamRout16 <routshubham2028@gmail.com>
2025-08-16 12:06:31 +0530

    edge case for optimal approach

--- Arrays/LongestConsecutiveSequence/LongestConsecutiveSequenceOpti.cpp
+++ Arrays/LongestConsecutiveSequence/LongestConsecutiveSequenceOpti.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 int LongestConsecutiveArrayOpti(vector<int> &nums){
   int n = nums.size();
+  // consider the edge case where the nums array is []
+  if(n == 0) return 0;
   int longest = 1;
   unordered_set<int> st;
   for(int i=0; i<n; i++){
