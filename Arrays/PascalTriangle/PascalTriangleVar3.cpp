7a13aa1fbfe3c78cebaf7c4498eac69718d3c6a5
ShubhamRout16 <routshubham2028@gmail.com>
2025-08-20 19:43:03 +0530

    variety 3 of pascal triangle solved

---
+++ Arrays/PascalTriangle/PascalTriangleVar3.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> generateRow(int row){
+  long long ans = 1;
+  vector<int> ansRow ;
+  ansRow.push_back(1);
+  for(int i=1; i<row; i++){
+    ans = ans * (row - i);
+    ans = ans / i;
+    ansRow.push_back(ans);
+  }
+  return ansRow;
+}
+vector<vector<int>> pascalTriangleVar3(int N){
+  vector<vector<int>> ans;
+  for(int i=1; i<=N; i++){
+    vector<int> temp = generateRow(i);
+    ans.push_back(temp);
+  }
+  return ans;
+}
+int main(){
+  int N;
+  cout << "Enter N : " << endl;
+  cin >> N;
+  vector<vector<int>> result = pascalTriangleVar3(N);
+
+  cout << "Pascal triangle will be" << endl;
+  for(auto &row : result){
+    for(auto &val : row){
+      cout << val << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
