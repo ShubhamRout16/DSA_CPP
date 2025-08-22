6266b33695739af47f7dc19800ba5e13eb01b431
ShubhamRout16 <routshubham2028@gmail.com>
2025-08-22 14:27:59 +0530

    total number of distinct elements in array of length n

---
+++ DSA_lab/lab2/Question 8/distinctElements.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+void distinctElements(int arr[] , int n){
+  int count = 0;
+  for(int i=0; i<n; i++){
+    int duplicateEl = 0;
+    for(int j=0; j<i; j++){
+      if(arr[i] == arr[j]){
+        duplicateEl = 1;
+        break;
+      }
+    }
+    if(duplicateEl == 0){
+      count++;
+    }
+  }
+  cout << "Total number of distinct elements : " << count << endl;
+}
+int main(){
+  int n;
+  cout << "Enter number of elements : " << endl;
+  cin >> n;
+  int arr[n];
+  cout << "Enter array elements : " << endl;
+  for(int i=0; i<n; i++){
+    cin >> arr[i];
+  }
+  cout << "Current array is : " << endl;
+  for(int i=0; i<n; i++){
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+  distinctElements(arr , n);
+  return 0;
+}
